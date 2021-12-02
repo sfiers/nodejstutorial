34dc61f9b5dfd8910e28505361f371bcab849dd9
sfiers <simon.fiers@outlook.com>
2021-12-02 15:19:02 +0100

    upload an image

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,21 +1,37 @@
 // var exec = require("child_process").exec;
-var querystring = require("querystring")
+var querystring = require("querystring"),
+    fs = require("fs"),
+    formidable = require("formidable");
 
 function start(response, postData) {
     console.log("Request handler 'start' was called.");
-
+    
     var body = '<html>'+
         '<head>'+
-        '<meta http-equiv="Content-Type" content="text/html; '+
-        'charset=UTF-8" />'+
+        '<meta http-equiv="Content-Type" '+
+        'content="text/html; charset=UTF-8" />'+
         '</head>'+
         '<body>'+
-        '<form action="/upload" method="post">'+
-        '<textarea name="text" rows="20" cols="60"></textarea>'+
-        '<input type="submit" value="Submit text" />'+
+        '<form action="/upload" enctype="multipart/form-data" '+
+        'method="post">'+
+        '<input type="file" name="upload">'+
+        '<input type="submit" value="Upload file" />'+
         '</form>'+
         '</body>'+
         '</html>';
+
+    // var body = '<html>'+
+    //     '<head>'+
+    //     '<meta http-equiv="Content-Type" content="text/html; '+
+    //     'charset=UTF-8" />'+
+    //     '</head>'+
+    //     '<body>'+
+    //     '<form action="/upload" method="post">'+
+    //     '<textarea name="text" rows="20" cols="60"></textarea>'+
+    //     '<input type="submit" value="Submit text" />'+
+    //     '</form>'+
+    //     '</body>'+
+    //     '</html>';
 
     response.writeHead(200, {"Content-Type": "text/html"});
     response.write(body);
@@ -40,13 +56,34 @@ function start(response, postData) {
     // return content;
 }
 
-function upload(response, postData) {
+function upload(response, request) {
     console.log("Request handler 'upload' was called.");
-    response.writeHead(200, {"Content-Type": "text.plain"});
-    response.write("You've sent:" + querystring.parse(postData).text);
-    response.end()
+
+    var form = new formidable.IncomingForm();
+    console.log("about to parse");
+    form.parse(request, function(error, fields, files) {
+        console.log("parsing done");
+        fs.rename(files.upload.path, "/tmp/test.png", function(error) {
+            if (error) {
+                console.log("eerr")
+                fs.unlink("/tmp/test.png");
+                fs.rename(files.upload.path, "/tmp/test.png");
+            }
+        });
+        response.writeHead(200, {"Content-Type": "text.html"});
+        response.write("Received image:<br/>");
+        response.write("<img src='/show' />");
+        response.end()
+    });
     // return "Hello Upload";
+}
+
+function show(response) {
+    console.log("Request handler 'show' was called.");
+    response.writeHead(200, {"Content-Type": "image/png"});
+    fs.createReadStream("./tmp/test.png").pipe(response);
 }
 
 exports.start = start;
 exports.upload = upload;
+exports.show = show;
--- server.js
+++ server.js
@@ -3,20 +3,22 @@ var url = require("url");
 
 function start(route, handle) {
   function onRequest(request, response) {
-    var postData = "";
+    // var postData = "";
     var pathname = url.parse(request.url).pathname;
     console.log("Request for " + pathname + " received.")
+    route(handle, pathname, response, request);
+  
 
-    request.setEncoding("utf8");
+    // request.setEncoding("utf8");
 
-    request.addListener("data", function(postDataChunk){
-      postData += postDataChunk;
-      console.log("Received POST data chunk '" + postDataChunk + "'.")
-    });
+    // request.addListener("data", function(postDataChunk){
+    //   postData += postDataChunk;
+    //   console.log("Received POST data chunk '" + postDataChunk + "'.")
+    // });
 
-    request.addListener("end", function() {
-      route(handle, pathname, response, postData)
-    })
+    // request.addListener("end", function() {
+    //   route(handle, pathname, response, postData)
+    // })
 
     // route(handle, pathname, response);
 
