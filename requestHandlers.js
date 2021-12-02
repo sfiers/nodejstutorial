5132607b5521af539662eba25364d34f45a320e2
sfiers <simon.fiers@outlook.com>
2021-12-02 12:11:01 +0100

    handling POST request

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,18 +1,51 @@
-function start() {
+// var exec = require("child_process").exec;
+var querystring = require("querystring")
+
+function start(response, postData) {
     console.log("Request handler 'start' was called.");
 
-    function sleep(milliSeconds) {
-        var startTime = new Date().getTime();
-        while (new Date().getTime() < startTime + milliSeconds);
-    }
+    var body = '<html>'+
+        '<head>'+
+        '<meta http-equiv="Content-Type" content="text/html; '+
+        'charset=UTF-8" />'+
+        '</head>'+
+        '<body>'+
+        '<form action="/upload" method="post">'+
+        '<textarea name="text" rows="20" cols="60"></textarea>'+
+        '<input type="submit" value="Submit text" />'+
+        '</form>'+
+        '</body>'+
+        '</html>';
 
-    sleep(10000);
-    return "Hello Start";
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write(body);
+    response.end();
+
+    // var content = "empty";
+
+    // exec("find /", { timeout: 10000, maxBuffer: 20000*1024 }, function (error, stdout, stderr){
+    //     // content = stdout;
+    //     response.writeHead(200, {"Content-Type": "text/plain"});
+    //     response.write(stdout);
+    //     response.end();
+    // });
+    // function sleep(milliSeconds) {
+    //     var startTime = new Date().getTime();
+    //     while (new Date().getTime() < startTime + milliSeconds);
+    // }
+
+    // sleep(10000);
+    // return "Hello Start";
+
+    // return content;
 }
 
-function upload() {
+function upload(response, postData) {
     console.log("Request handler 'upload' was called.");
-    return "Hello Upload";
+    response.writeHead(200, {"Content-Type": "text.plain"});
+    response.write("You've sent:" + querystring.parse(postData).text);
+    response.end()
+    // return "Hello Upload";
 }
 
 exports.start = start;
--- server.js
+++ server.js
@@ -3,16 +3,30 @@ var url = require("url");
 
 function start(route, handle) {
   function onRequest(request, response) {
+    var postData = "";
     var pathname = url.parse(request.url).pathname;
     console.log("Request for " + pathname + " received.")
+
+    request.setEncoding("utf8");
+
+    request.addListener("data", function(postDataChunk){
+      postData += postDataChunk;
+      console.log("Received POST data chunk '" + postDataChunk + "'.")
+    });
+
+    request.addListener("end", function() {
+      route(handle, pathname, response, postData)
+    })
+
+    // route(handle, pathname, response);
 
     // route(handle, pathname);
 
     // console.log("Request received");
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    var content = route(handle, pathname)
-    response.write(content);
-    response.end();
+    // response.writeHead(200, {"Content-Type": "text/plain"});
+    // var content = route(handle, pathname)
+    // response.write(content);
+    // response.end();
   }
 
   http.createServer(onRequest).listen(8888);
