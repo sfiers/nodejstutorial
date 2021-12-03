4277da4475525af922fdf6c218b90d58c2c3dc43
sfiers <simon.fiers@outlook.com>
2021-12-03 11:30:46 +0100

    learnyounode 7/13

---
+++ learnyounode/http-client.js
@@ -0,0 +1,8 @@
+'use strict'
+const http = require('http')
+
+http.get(process.argv[2], (response) => {
+    response.setEncoding('utf8');
+    response.on('data', console.log);
+    response.on('error', console.log);
+}).on('error', console.error)
